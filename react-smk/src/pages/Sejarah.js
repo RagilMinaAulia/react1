9666eb1ef1bdf0366b15441e1270611e8cb4d922
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-03-17 13:37:31 +0700

    Menambahkan Gambar

--- react-smk/src/pages/Sejarah.js
+++ react-smk/src/pages/Sejarah.js
@@ -3,6 +3,7 @@ function Sejarah() {
       <div className="App">
         <h1>Sejarah SMK</h1>
         <p>Isi sejarah</p>
+        <img src="images/logo.png" alt="logo" />
       </div>
     );
   }
