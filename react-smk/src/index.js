3ad5e99d27e75cb35cff892d2cbfc1672924f6d6
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-03-17 13:31:47 +0700

    Komponen

--- react-smk/src/index.js
+++ react-smk/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Kontak from './pages/Kontak';
+import Nav from './pages/Nav';
+import Sejarah from './pages/Sejarah';
+import Tentang from './pages/Tentang';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Nav />
     <App />
     <Kontak />
+    <Sejarah />
+    <Tentang />
   </React.StrictMode>
 );
 
