2a5b6eeddfbcb108ee44ae3d26ee8247cdfc1cf0
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-03-17 13:31:28 +0700

    Komponen

---
+++ react-smk/src/pages/Sejarah.js
@@ -0,0 +1,10 @@
+function Sejarah() {
+    return (
+      <div className="App">
+        <h1>Sejarah SMK</h1>
+        <p>Isi sejarah</p>
+      </div>
+    );
+  }
+  
+  export default Sejarah;
---
+++ react-smk/src/pages/Tentang.js
@@ -0,0 +1,10 @@
+function Tentang() {
+    return (
+      <div className="App">
+        <h1>Tentang SMK</h1>
+        <p>Isi tentang SMK</p>
+      </div>
+    );
+  }
+  
+  export default Tentang;
